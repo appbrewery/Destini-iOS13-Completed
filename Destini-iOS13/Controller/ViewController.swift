212309d1009c0e684bfce4ba1fa4f4afee9e4308
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-09 13:10:11 +0100

    Step 4 Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    //This will track where the user is in the storyline.
+    var storyNumber = 0
+    
     let stories = [
         Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
@@ -23,15 +26,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(stories[0].choice1, for: .normal)
-        choice2Button.setTitle(stories[0].choice2, for: .normal)
+       updateUI()
 
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
         
+        let userChoice = sender.currentTitle!
+        
+        //Find the current story based on the current value of storyNumber.
+        let currentStory = stories[storyNumber]
+        
+        //If the user chose choice1 go to story 1 else go to story 2.
+        if userChoice == currentStory.choice1 {
+            storyNumber = 1
+        } else if userChoice == currentStory.choice2 {
+            storyNumber = 2
+        }
+        
+        updateUI()
      
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     }
     
 }
