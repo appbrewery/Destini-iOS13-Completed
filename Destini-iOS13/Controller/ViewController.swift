7f55949ae58ebb51d96de9f748d40cc8443e109f
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-09 12:54:07 +0100

    Step 2 Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
 
     }
 
