72837e32d655831bb5ee98c7014e2e0b5eab2290
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-09 13:03:45 +0100

    Step 3 Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,21 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
 
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
         
+     
     }
     
 }
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -8,3 +8,8 @@
 
 import Foundation
 
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+}
