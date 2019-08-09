de9a06ea230690e69ae0bb788b3f5913db1aef3a
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-09 16:18:32 +0100

    Step 5 Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,14 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    //This will track where the user is in the storyline.
-    var storyNumber = 0
-    
-    let stories = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
+   var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,26 +25,19 @@ class ViewController: UIViewController {
 
     @IBAction func choiceMade(_ sender: UIButton) {
         
-        let userChoice = sender.currentTitle!
-        
-        //Find the current story based on the current value of storyNumber.
-        let currentStory = stories[storyNumber]
-        
-        //If the user chose choice1 go to story 1 else go to story 2.
-        if userChoice == currentStory.choice1 {
-            storyNumber = 1
-        } else if userChoice == currentStory.choice2 {
-            storyNumber = 2
-        }
+        //Calls nextStory in the storyBrain and sends over the title of the button that was chosen by the user.
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
         
         updateUI()
      
     }
     
     func updateUI() {
-        storyLabel.text = stories[storyNumber].title
-        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
-        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+        
+        //Now delegating all the data fetching to the storyBrain.
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 }
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -1,11 +1,50 @@
-//
-//  StoryBrain.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
-import Foundation
-
-
+    //
+    //  StoryBrain.swift
+    //  Destini-iOS13
+    //
+    //  Created by Angela Yu on 08/08/2019.
+    //  Copyright © 2019 The App Brewery. All rights reserved.
+    //
+    
+    import Foundation
+    
+    struct StoryBrain {
+        
+        //Tracking the current story number. This is used to pick out the right story from the stories Array.
+        var storyNumber = 0
+        
+        //All the story data moved over.
+        let stories = [
+            Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+            Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+            Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+        ]
+        
+        func getStoryTitle() -> String {
+            return stories[storyNumber].title
+        }
+        
+        func getChoice1() -> String {
+            return stories[storyNumber].choice1
+        }
+        
+        func getChoice2() -> String {
+            return stories[storyNumber].choice2
+        }
+        
+        //Mutating function as we're changing the storyNumber property.
+        mutating func nextStory(userChoice: String) {
+            
+            //All logic is still exactly the same as before, now moved to storyBrain to act as a seperate manager of data.
+            let currentStory = stories[storyNumber]
+            
+            if userChoice == currentStory.choice1 {
+                storyNumber = 1
+            } else if userChoice == currentStory.choice2 {
+                storyNumber = 2
+            }
+        }
+        
+    }
+    
+    
